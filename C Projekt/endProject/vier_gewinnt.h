1135d0bc5453d7b1d2e735d0b090e80f79e77765
Sophia <sophia.lichtenberg@stud.uni-regensburg.de>
2019-02-20 13:32:56 +0100

    added comments

--- C Projekt/endProject/vier_gewinnt.h
+++ C Projekt/endProject/vier_gewinnt.h
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <iostream>
 #include <string.h>
-#include <limits>
-#include <stdlib.h>
-#include <ctype.h>
 using namespace std;
 
 #ifndef vier_gewinnt
 #define vier_gewinnt
 
+//Dimensions of the Gameboard
 const int ROWS = 6, COLS = 7;
 int x, y;
+// Boolean, that checks who's turn it is
 bool player_1 = false;
+// for them main game loop
 bool gameRunning = true;
+
+// checks if a player connected 4 of his Symbols 
+// in a vertical, horizontal or diagonal 
+// direction and returns true if he did otherwise return false
 
 bool checkWin(char gameBoard[][COLS], char playerSymbol){
 
@@ -52,6 +56,9 @@ bool checkWin(char gameBoard[][COLS], char playerSymbol){
   return false;
 }
 
+// print board after every turn with the Numbers at the side
+// for the Columns and Rows
+
 void printBoard(char gameBoard[][COLS]){
   cout << "      ";
   for (y = 0; y < COLS; y++){
@@ -67,30 +74,57 @@ void printBoard(char gameBoard[][COLS]){
   }
 }
 
-int checkUserInput(int player ,char playerSymbol, int x) {
+// check if user input is longer than 1 (because we expect a digit, so a string of the length 1) -> rekrusive again
+// check if that is a digit --> if not -> rekrusive again
+// check if Input Number is inside the Game board --> if not --> rekrusive again
+// read the input as as String for the check and returns the Integer Value that the user enters
+// (for Row = 1 or Column = 2)
+
+int checkUserInput(int player ,char playerSymbol, int row_or_col) {
   string input;
 
-  if (x == 1){
+  if (row_or_col == 1){
     cout << "Row: ";
-    cin >> input;
   }
-  if (x == 2){
+  if (row_or_col == 2){
     cout << "Column: ";
-    cin >> input;
   }
+
+  cin >> input;
 
   if(input.length() == 1){
     if(isdigit(input[0])){
-      return atoi(input.c_str());;
+      // convert string to Integer value
+      int temp = atoi(input.c_str());
+
+      if (row_or_col == 1){
+        if (temp >= 0 && temp < ROWS){
+          return temp;
+        } else {
+          return checkUserInput(player, playerSymbol, row_or_col);
+        }
+      }
+      if (row_or_col == 2){
+        if (temp >= 0 && temp < COLS){
+          return temp;
+        } else {
+          return checkUserInput(player, playerSymbol, row_or_col);
+        }
+      }
     } else {
-      return checkUserInput(player, playerSymbol, x);
+      return checkUserInput(player, playerSymbol, row_or_col);
     }
   } else {
-    return checkUserInput(player, playerSymbol, x);
+    return checkUserInput(player, playerSymbol, row_or_col);
   }
 
 }
 
+// function parameter says which player turn it is (Player 1 has the Symbol 'x' and Player 2 the Symbol 'o')
+// read the user input (with checking if valid input)
+// EXTRA Check if the turn a player wants to do, was already made --> if yes --> function turn again (rekrusive)
+// For every turn function 'checkWin' checks if 4 Symbols got connected, if yes --> gameLoop ends
+// if not --> change the player
 
 void turn(char gameBoard[][COLS], int player){
   char playerSymbol;
@@ -107,19 +141,15 @@ void turn(char gameBoard[][COLS], int player){
   userRow = checkUserInput(player, playerSymbol, 1);
   userCol = checkUserInput(player, playerSymbol, 2);
   
-  if (userRow >= 0 && userCol >= 0 && userRow < ROWS && userCol < COLS){
-    if(gameBoard[userRow][userCol] == '_') {
-      cout << "Set at Position -> " << "(" << userRow << ", " << userCol << ")\n\n";
-      gameBoard[userRow][userCol] = playerSymbol;
-      printBoard(gameBoard);
-    } else {
-      cout << "You can't do this turn\n";
-      turn(gameBoard, player);
-    }
+  if(gameBoard[userRow][userCol] == '_') {
+    cout << "Set at Position -> " << "(" << userRow << ", " << userCol << ")\n\n";
+    gameBoard[userRow][userCol] = playerSymbol;
+    printBoard(gameBoard);
   } else {
     cout << "You can't do this turn\n";
     turn(gameBoard, player);
   }
+  
 
   if (checkWin(gameBoard, playerSymbol)){
     gameRunning = false;
@@ -133,6 +163,10 @@ void turn(char gameBoard[][COLS], int player){
     }
   }
 };
+
+// initialize the game
+// set up the gameBoard and start the turn with player 1
+// main game Loop --> gameRunning --> change the players turns until game ends
 
 void init_vier_gewinnt() {
   char gameBoard[ROWS][COLS];
